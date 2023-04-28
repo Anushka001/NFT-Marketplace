aa1d47ac4390552fb971cbfacba9a91615fc2f27
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2023-04-28 16:05:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// navbar toggle
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const menuCloseBtn = document.querySelector("[data-nav-close-btn]");
+const menuOpenBtn = document.querySelector("[data-nav-open-btn]");
+
+const elemArr = [overlay, menuCloseBtn, menuOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+// navbar toggle when navbar link clicked
+
+const navbarLinks = document.querySelectorAll("[data-navlink]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+// active class for header and back-to-top button when window will scroll 100px from top
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
